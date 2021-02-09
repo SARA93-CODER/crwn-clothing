3e22e502eb405f04084134e132261ab313e6b461
SARA93-CODER <sara.inbashi@hotmail.com>
2021-02-09 10:38:26 +0200

    save changes

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,2 +1,9 @@
 import React from 'react';
 import './sign-in-nd-sign-up.styles.scss';
+
+
+const SignInAndSignUp = () => (
+    <div className='sign-in-and-sign-up'>SIGN IN</div>
+); 
+
+export default SignInAndSignUp;
