6df4a78f798a105b9423c1cfe96bc272f02b9ff0
SARA93-CODER <sara.inbashi@hotmail.com>
2021-02-09 09:53:29 +0200

    make it works

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import { Route, Switch } from 'react-router-dom';
-
 import Header from './Components/header/header.component';
 
 
