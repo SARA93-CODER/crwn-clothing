cec6707e714458016346fe08820d79c813a6ab86
SARA93-CODER <sara.inbashi@hotmail.com>
2021-02-15 11:53:51 +0200

    update changes

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import './sign-in-and-sign-up.styles.scss';
+import React from "react";
+import "./sign-in-and-sign-up.styles.scss";
 
-import SignIn from '../../Components/sign-in/sign-in.component';
+import SignIn from "../../Components/sign-in/sign-in.component";
 
+import SignUp from "../../Components/sign-up/sign-up.component";
 
 const SignInAndSignUp = () => (
-    <div className='sign-in-and-sign-up'>
-        <SignIn />
-    </div>
-); 
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
 
 export default SignInAndSignUp;
