7120d570d613315fd88e30d6cd8e50ad8afcc095
SARA93-CODER <sara.inbashi@hotmail.com>
2021-02-16 13:53:24 +0200

    update chnges

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import { provider } from 'react-redux';
+import store from './redux/store';
+
 import './index.css';
 import App from './App';
 
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>,
+  //we passing the (store) to the provider, so we can access the all reducers and props needed inside it
+  <provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  <provider>,
   document.getElementById('root')
 );
-
-
-
