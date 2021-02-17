e958c00724a541b6a4ca329df7c62d1d7c002e74
SARA93-CODER <sara.inbashi@hotmail.com>
2021-02-17 13:27:08 +0200

    update chnges

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
+import { UserActionTypes } from "./user.types";
+
 const INITIAL_STATE = {
   currentUser: null,
 };
 // we make a slice reducer that we will store it at the end in the root reducer which will be stored in turn in the store of the provider.
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
