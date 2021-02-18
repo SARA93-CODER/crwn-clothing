826f3d1830f6997f8007082b86d8616b488add98
SARA93-CODER <sara.inbashi@hotmail.com>
2021-02-18 11:31:32 +0200

    update changes

--- src/Components/header/header.component.jsx
+++ src/Components/header/header.component.jsx
@@ -7,8 +7,9 @@ import { auth } from "../../firebase/firebse.utils";
 import { connect } from "react-redux";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { CartDropdown } from "../cart-dropdown/cart-dropdown.component";
 
-import CartIcon from "../cart-icon/cart-icon.component";
+import { CartIcon } from "../cart-icon/cart-icon.component";
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -43,6 +44,7 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
