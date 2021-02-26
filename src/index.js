5179eac47725ff6ecb3d4d00c4c2f80f87649bb4
SARA93-CODER <sara.inbashi@hotmail.com>
2021-02-26 19:38:18 +0200

    update changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
+import { persistGate } from "redux-persist/integration/react";
+
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 import "./index.css";
 import App from "./App";
@@ -12,7 +14,9 @@ ReactDOM.render(
   //we passing the (store) to the provider, so we can access the all reducers and props needed inside it
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <persistGate persistor={persistor}>
+        <App />
+      </persistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,25 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import CartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
-//we make a gaint reducer that contains all the small reducers that the app needs by using combineReducers func.
-export default combineReducers({
+import { persistReducer } from "redux-persist";
+//we tell the redux persist to use the locl storage as a default storage
+import storage from "redux-persist/lib/storage";
+
+//an json object that represent possible configuration for redux-persist to use
+const persistConfig = {
+  key: ShadowRoot,
+  storage,
+  //we put the reducers we want to persist inside of the whitelist array, and because the user is persisted in the firebase, so we gonna put only the cart reducer in the app.
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: CartReducer,
+  reducer: directoryReducer,
 });
+
+//we make a gaint reducer that contains all the small reducers that the app needs by using combineReducers func.
+export default persistReducer(persistConfig, rootReducer);
