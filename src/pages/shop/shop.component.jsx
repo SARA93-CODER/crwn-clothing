a959107706213e62ea443f66d8f631e4d9212636
SARA93-CODER <sara.inbashi@hotmail.com>
2021-03-11 13:07:11 +0200

    update changes

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CollectionsOverview from "../../Components/collections-overview/collections-overview.component";
 
-import { Route } from "react-router-dom";
-import CollectionPage from "../collection/collection.component";
+import { connect } from "react-redux";
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
-const ShopPage = ({ match }) => {
-  <div className="shop-page">
-    {/*we put match.path (as a variable) in route, because in this way we make it
+import { Route } from "react-router-dom";
+import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
+import CollectionPageContainer from "../collection/collection.container";
+
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
+    fetchCollectionsStart();
+  }, [fetchCollectionsStart]);
+
+  return (
+    <div className="shop-page">
+      {/*we put match.path (as a variable) in route, because in this way we make it
     more flexible if we wnt to reuse it in onother place. but we can put
     (/shop/...)*/}
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    {/*the following expression allows us to access the categoryId as a parameter on the match object */}
-    <Route
-      exact
-      path={`${match.path}/:collectionId`}
-      component={CollectionPage}
-    />
-  </div>;
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      {/*the following expression allows us to access the categoryId as a parameter on the match object */}
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  );
 };
 
-export default ShopPage;
+const mapDispatchToProps = (dispatch) => ({
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
